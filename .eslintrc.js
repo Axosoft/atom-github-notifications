9b1d12b018e808af74a6f0bfaf84b919853084d5
Christopher Bargren <cbargren@gmail.com>
2017-03-02 20:18:45 -0700

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "globals": {
+      "atom": true
+    },
+    settings: {
+      'import/core-modules': ['atom']
+    }
+};
---
+++ spec/atom-github-notifications-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGithubNotificationsView from '../lib/atom-github-notifications-view';
+
+describe('AtomGithubNotificationsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
