bf0214e2acaf3c22fac6573c86d90389297cf998
Christopher Bargren <cbargren@gmail.com>
2017-03-06 17:02:58 -0700

    Display title and body for fetched pull requests/issues
    Also fixed an issue where plain newlines are swallowed by atom's markdown editor in the body of the subject of a notification.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ],
-    "globals": {
-      "atom": true
-    },
-    settings: {
-      'import/core-modules': ['atom']
-    }
+  extends: 'airbnb-base',
+  plugins: ['import'],
+  globals: {
+    atom: true,
+  },
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+  },
+  settings: {
+    'import/core-modules': ['atom'],
+  },
 };
